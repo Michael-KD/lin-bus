6ac30e516b11a939f321706c520c1c51a2d678e7
gnatyelir <hrileytang@gmail.com>
2024-02-12 19:46:51 -0800

    setup platformio

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
+#include <Arduino.h>
 #include "lin_bus.h"
 
 const int BAUD_RATE = 19200;
 
 
 void setup() {
-    LIN lin(&Serial1, BAUD_RATE);
+  Serial.begin(9600);
 }
 
 void loop() {
-    
+  Serial.println("hello world");
 }
