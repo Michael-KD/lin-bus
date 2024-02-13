f163c6fb080b9158c76c0de91acf54fbcf2704d6
gnatyelir <hrileytang@gmail.com>
2024-02-12 20:21:23 -0800

    pins and ids

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,27 @@
 #include <Arduino.h>
 #include "lin_bus.h"
 
-const int BAUD_RATE = 19200;
+const uint32_t BAUD_RATE = 19200;
+
+//pins
+const uint8_t LIN_RXD = 0;
+const uint8_t LIN_TXD = 1;
+const uint8_t LIN_MCP_RESET = 2;
+const uint8_t LIN_CS = 3;
+
+//ids (no particular order lol)
+const uint8_t CONTROLLER_ID = 0x3a;
+const uint8_t NODE_ID = 0x3b;
+
+LIN lin(&Serial1, 19200);
 
 
 void setup() {
   Serial.begin(9600);
+  pinMode(LIN_CS, OUTPUT);
+  digitalWrite(LIN_CS, HIGH);
 }
 
 void loop() {
-  Serial.println("hello world");
+  
 }
