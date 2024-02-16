222fa0eeee14a0eb1b6e64cc50b6334fce64c7f2
gnatyelir <hrileytang@gmail.com>
2024-02-15 23:12:24 -0800

    converted source file to verbose names for safety

--- include/lin.h
+++ include/lin.h
@@ -7,9 +7,9 @@
 namespace LIN {
     uint8_t parity(uint8_t id);
 
-    class LIN_Master {
+    class Master {
         public:
-            LIN_Master(HardwareSerial* serialPort, uint32_t baudRate);
+            Master(HardwareSerial* serialPort, uint32_t baudRate);
             uint8_t* requestData(uint8_t id); //send a header
         private:
             HardwareSerial* _serial;
@@ -17,9 +17,9 @@ namespace LIN {
             void generateHeader(uint8_t id, uint8_t* frame);
     };
 
-    class LIN_Puppet {
+    class Puppet {
         public:
-            LIN_Puppet(HardwareSerial* serialPort, uint8_t id, uint32_t baudRate);
+            Puppet(HardwareSerial* serialPort, uint8_t id, uint32_t baudRate);
             void reply(uint8_t* data); //respond to a header
             bool getDataRequested(); //gets header from master; returns true if header is itself
         private:
--- src/lin.cpp
+++ src/lin.cpp
@@ -1,21 +1,19 @@
 #include "lin.h"
 
-using namespace LIN;
-
 //parity should be bits 6, 7, all other bits are 0
-uint8_t parity(uint8_t id) {
+uint8_t LIN::parity(uint8_t id) {
     int bit6 = ((id >> 0) + (id >> 1) + (id >> 2) + (id >> 4)) & 1;
     int bit7 = ~((id >> 1) + (id >> 3) + (id >> 4) + (id >> 5)) & 1;
     return ((bit6 | (bit7 << 1)) << 6);
 }
 
-LIN_Master::LIN_Master(HardwareSerial* serialPort, uint32_t baudRate) {
+LIN::Master::Master(HardwareSerial* serialPort, uint32_t baudRate) {
     _serial = serialPort;
     this->baudRate = baudRate;
     _serial->begin(baudRate);
 }
 
-uint8_t* LIN_Master::requestData(uint8_t id) {
+uint8_t* LIN::Master::requestData(uint8_t id) {
     uint8_t headerFrame[4] = {0};
     generateHeader(id, headerFrame);
 
@@ -31,7 +29,7 @@ uint8_t* LIN_Master::requestData(uint8_t id) {
    return NULL;
 }
 
-void LIN_Master::generateHeader(uint8_t id, uint8_t* frame) {
+void LIN::Master::generateHeader(uint8_t id, uint8_t* frame) {
     // break, 13 dominant bits (nominal) followed by a break delimiter of one bit (nominal) recessive
     frame[0] = 0x3f;
     frame[1] = 0xff;
@@ -45,34 +43,34 @@ void LIN_Master::generateHeader(uint8_t id, uint8_t* frame) {
 
 // =============================================================================================== //
 
-LIN_Puppet::LIN_Puppet(HardwareSerial* serialPort, uint8_t id, uint32_t baudRate) {
+LIN::Puppet::Puppet(HardwareSerial* serialPort, uint8_t id, uint32_t baudRate) {
     _serial = serialPort;
     this->id = id;
     this->baudRate = baudRate;
     _serial->begin(baudRate);
 }
 
-bool LIN_Puppet::getDataRequested() {
+bool LIN::Puppet::getDataRequested() {
     //checks bus buffer for input, add to internal buffer
     //if buffer is complete, check PID and handle accordingly
     uint8_t id = 0; //TODO
-    LIN_Puppet::compareID(id);
+    LIN::Puppet::compareID(id);
 
     return false;
 }
 
-void LIN_Puppet::reply(uint8_t* data) {
+void LIN::Puppet::reply(uint8_t* data) {
     uint8_t frame[DATA_SIZE + 1] = {0};
-    LIN_Puppet::generateResponse(data, frame);
+    LIN::Puppet::generateResponse(data, frame);
 }
 
-void LIN_Puppet::generateResponse(uint8_t* data, uint8_t* frame) {
+void LIN::Puppet::generateResponse(uint8_t* data, uint8_t* frame) {
     frame[0] = 0;
     //populate 
     //generate CRC
 }
 
-bool LIN_Puppet::compareID(uint8_t id) {
+bool LIN::Puppet::compareID(uint8_t id) {
     //check if ID is the same as this puppet's ID, check parity
     if (parity(id) == (id & 0xc0)) {
         if (id == this->id) {
